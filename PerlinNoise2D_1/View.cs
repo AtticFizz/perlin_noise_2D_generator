d5d3da05255cc2f4c39c9014c430eac24c66f24d
AtticFizz <66317048+AtticFizz@users.noreply.github.com>
2023-03-24 14:39:42 +0200

    Added custom terrain generator

--- PerlinNoise2D_1/View.cs
+++ PerlinNoise2D_1/View.cs
@@ -1,6 +1,7 @@
 using System.Numerics;
 using System;
 using NoiseGenerator2;
+using TerrainGenerator;
 
 namespace PerlinNoise2D_1;
 
@@ -16,7 +17,7 @@ public partial class View : Form
         Debug.Initialize(debugTextbox);
         Debug.SetVisibility(DEBUG_VISIBLE);
 
-        PerlinNoise2D.Seed = 12345;
+        PerlinNoise2D.Seed = 1234;
         _image = UV.Create(ClientSize.Width, ClientSize.Height);
     }
 
@@ -31,20 +32,30 @@ public partial class View : Form
         Renderer.Draw(_image, e);
     }
 
+    private bool createOnce = true;
+
     private void framerateTimer_Tick(object sender, EventArgs e)
     {
         //double[,] noise = PerlinNoise2D.Create();
         //_image = PerlinNoise2D.ToBitmap(noise);
 
-        double[,] noise = new double[ClientSize.Width, ClientSize.Height];
-        for (int y = 0; y < ClientSize.Height; y++)
+        //double[,] noise = new double[ClientSize.Width, ClientSize.Height];
+        //for (int y = 0; y < ClientSize.Height; y++)
+        //{
+        //    for (int x = 0; x < ClientSize.Width; x++)
+        //    {
+        //        noise[x, y] = PerlinNoise2D.CreateNoiseValue(x * 0.01, y * 0.01);
+        //    }
+        //}
+        //_image = PerlinNoise2D.ToBitmap(noise);
+
+        if (createOnce)
         {
-            for (int x = 0; x < ClientSize.Width; x++)
-            {
-                noise[x, y] = PerlinNoise2D.CreateNoiseValue(x * 0.01, y * 0.01);
-            }
+            CustomTerrainGenerator.TresholdSea = 0.000005;
+            CustomTerrainGenerator.TresholdSurface = 0.55;
+            _image = CustomTerrainGenerator.Create();
+            createOnce = false;
         }
-        _image = PerlinNoise2D.ToBitmap(noise);
 
         Refresh();
     }
---
+++ TerrainGenerator/CustomTerainGenerator.cs
@@ -0,0 +1,51 @@
+﻿using NoiseGenerator2;
+using System.Drawing;
+using System.Runtime.Versioning;
+
+namespace TerrainGenerator;
+
+public static class CustomTerrainGenerator
+{
+    public static double TresholdSea { get; set; }
+    public static double TresholdSurface { get; set; }
+
+    public static Color ColorSea { get; set; }
+    public static Color ColorSurface { get; set; }
+    public static Color ColorMountains { get; set; }
+
+    static CustomTerrainGenerator()
+    {
+        TresholdSea = 0.95;
+        TresholdSurface = 0.5;
+
+        ColorSea = Color.FromArgb(255, 102, 205, 170);
+        ColorSurface = Color.FromArgb(255, 107, 142, 35);
+        ColorMountains = Color.FromArgb(255, 220, 220, 220);
+    }
+
+    [SupportedOSPlatform("windows")]
+    public static Bitmap Create()
+    {
+        double[,] noise = PerlinNoise2D.Create();
+        int width = noise.GetLength(0);
+        int height = noise.GetLength(1);
+        Bitmap noiseImage = new Bitmap(width, height);
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Color pixelColor = ColorMountains;
+                if (noise[x, y] <= TresholdSea)
+                    pixelColor = ColorSea;
+                else if (noise[x, y] <= TresholdSurface)
+                    pixelColor = ColorSurface;
+                noiseImage.SetPixel(x, y, pixelColor);
+            }
+        }
+
+        return noiseImage;
+    }
+
+
+}
